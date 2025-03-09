03403988195ffeb06075865e7295c8bc2ae66b99
kage1020 <4624528@ed.tus.ac.jp>
2025-03-09 16:22:15 +0900

    add ogp to home

--- home/astro.config.mjs
+++ home/astro.config.mjs
@@ -8,8 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
